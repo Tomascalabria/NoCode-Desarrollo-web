7c120a8ecab3d8f7e8b53dc02c17ea83cd86c852
Tomascalabria <totocalabria.tc@gmail.com>
2021-07-25 12:46:59 -0300

    arreglando rutas del menu

--- Script/menu.js
+++ Script/menu.js
@@ -34,4 +34,9 @@
             
             }
         
-         
+            document.getElementById('contact__menu').click = function () {
+              hideMenu();
+           
+           }
+       
+        
